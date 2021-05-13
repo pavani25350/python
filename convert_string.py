1554a7d097ef969c26df64a4b7453d265b2f13ac
pavani.uppala <79527235+pavani25350@users.noreply.github.com>
2021-05-13 20:46:50 +0530

    Add files via upload
    """
    You are given a string STR. You have to convert the first alphabet of each word in a string to UPPER CASE.
    For example: If the given string STR = ”I am a student of the third year.” so you have to transform this string to
    ”I Am A Student Of The Third Year.”
    """

---
+++ convert_string.py
@@ -0,0 +1,16 @@
+a=input()
+x=a.split()
+a=[]
+for i in x:
+    a.append(i.capitalize())
+fs=' '.join(a)
+print(fs)
+
+
+
+
+"""
+You are given a string STR. You have to convert the first alphabet of each word in a string to UPPER CASE.
+For example: If the given string STR = ”I am a student of the third year.” so you have to transform this string to
+”I Am A Student Of The Third Year.”
+"""
